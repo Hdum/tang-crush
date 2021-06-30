6f04003da606218134ac9324a223372eba8e729f
Hdum <86713832+Hdum@users.noreply.github.com>
2021-06-30 16:09:03 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,16 @@
 const CONFIG = {
-    introTitle: 'Djt me m NAM à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    introTitle: 'Djt me m NAM GAY à!',
+    introDesc: `Học mẹ m đi pes ít thôi,
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
+    title: 'Phải chăng m đã gay ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    btnYes: 'Tao là PHNAM và t bị gay <33',
+    btnNo: 'Không :3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao m lại gay <3',
+    btnReply: 'Gayyyy <3',
+    reply: 'gay thì gay mà không gayy thì gay <33333333',
+    mess: 't biết mà 🥰. m gay vl 😘😘',
+    messDesc: 'Học thôi.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
